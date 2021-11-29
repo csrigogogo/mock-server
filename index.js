66628b372c494ef2b77ed0f90bb5cc12d49a8106
csrigogogo <cri.sri@outlook.com>
2021-11-29 16:38:27 +0800

    [feat] Content-Disposition响应图片

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express")
 const app = express()
+const contentDisposition = require("content-disposition")
 
 app.get("/", (req, res) => {
   res.send("Hello World!")
 })
+app.get("/logo", (req, res) => {
+  const file = `${__dirname}/logo.png`
+  //  预览图片
+  res.setHeader(
+    "Content-Disposition",
+    contentDisposition(file, { type: "inline" })
+  )
+  res.sendFile(file)
+  //  下载图片
+  //   res.setHeader("Content-Disposition", contentDisposition(file, { type: 'attachment' }))
+  //   res.sendFile(file)
+})
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000")
 })
